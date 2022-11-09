344982f0d34beda5057cd1b8602828877fce3c77
Rita Camacho <a104439@alunos.uminho.pt>
2022-11-09 11:54:06 +0000

    Realização da quarta tarefa

--- src/Tarefa4_2022li1g002.hs
+++ src/Tarefa4_2022li1g002.hs
@@ -11,4 +11,10 @@ module Tarefa4_2022li1g002 where
 import LI12223
 
 jogoTerminou :: Jogo -> Bool
-jogoTerminou = undefined
+jogoTerminou (Jogo (Jogador (x,y)) (Mapa l p))
+    | x < 0 || x >= length (snd (head p)) || y < 0 || y >= length p = True
+    | otherwise = let terreno = fst (p !! y); obstaculo = snd (p !! y) !! x in
+        case terreno of
+            Relva -> False
+            Estrada _ -> obstaculo == Carro
+            Rio _ -> obstaculo == Nenhum
