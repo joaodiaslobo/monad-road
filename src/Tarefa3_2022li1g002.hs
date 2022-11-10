f84fa052aec9c26d46f2dc20342608f28db89f73
João Lobo <a104356@alunos.uminho.pt>
2022-11-10 12:39:18 +0000

    Início da documentação da terceira tarefa

--- src/Tarefa3_2022li1g002.hs
+++ src/Tarefa3_2022li1g002.hs
@@ -10,11 +10,37 @@ module Tarefa3_2022li1g002 where
 
 import LI12223
 
-animaJogo :: Jogo -> Jogada -> Jogo
+{- | A função __não recursiva__ 'animaJogo' movimenta os obstáculos (de acordo com a velocidade) do terreno em que se encontram), e o personagem, de acordo com a jogada dada.
+
+=== Exemplo de utilização:
+
+>>>animaJogo (Jogo (Jogador (2,2)) (Mapa 5 [(Relva, [Arvore, Nenhum, Arvore, Nenhum, Arvore]),(Estrada (-1), [Nenhum, Nenhum, Nenhum, Carro, Carro]),(Relva, [Arvore, Nenhum, Nenhum, Arvore, Arvore])])) (Move Esquerda)
+Jogo (Jogador (1,2)) (Mapa 5 [(Relva,[Arvore,Nenhum,Arvore,Nenhum,Arvore]),(Estrada (-1),[Nenhum,Nenhum,Carro,Carro,Nenhum]),(Relva,[Arvore,Nenhum,Nenhum,Arvore,Arvore])])
+-}
+animaJogo :: Jogo -- ^Estado do jogo antes da jogada.
+    -> Jogada -- ^A jogada em si, 'Nenhum' ou 'Move' 'Direcao'.
+    -> Jogo -- ^Jogo após a jogada dada.
 animaJogo (Jogo p (Mapa l ls)) j = Jogo jogador (Mapa l linhas)
     where jogador = moveJogador p j ls; linhas = animaObstaculos ls
 
-moveJogador :: Jogador -> Jogada -> [(Terreno, [Obstaculo])] -> Jogador
+{- | A função __não recursiva__ 'moveJogador' recebe 'Jogador' com as 'Coordenadas' antes das execução da função, uma 'Jogada' e a lista de linhas do mapa. Para o jogador se mover, têm de ser respeitadas as seguintes condições:
+
+* O jogador __não__ pode sair do mapa (exceto quando está num tronco)
+* O jogador __não__ pode ir para "cima" de árvores, ou seja, quando há uma árvore no caminho do jogador este fica parado no lugar
+* Quando a jogada é parado e o jogador se encontra na posição do tronco, este move-se junto com o tronco (|v| unidades na direção do rio)
+
+=== Exemplo de utilização:
+
+>>>moveJogador (Jogador (1,1)) (Move Cima) [(Relva, [Arvore, Nenhum, Arvore]),(Relva, [Nenhum,Nenhum,Nenhum])]
+Jogador (1,0)
+
+>>>moveJogador (Jogador (0,1)) (Move Cima) [(Relva, [Arvore, Nenhum, Arvore]),(Relva, [Nenhum,Nenhum,Nenhum])]
+Jogador (0,1)
+-}
+moveJogador :: Jogador -- ^Estado do jogador antes da jogada.
+    ->Jogada -- ^A jogada em si, 'Nenhum' ou 'Move' 'Direcao'.
+    -> [(Terreno, [Obstaculo])] -- ^As linhas do mapa.
+    -> Jogador -- ^Estado do jogador pós jogada.
 moveJogador (Jogador (x,y)) j p =
     case j of
         Parado ->
@@ -26,15 +52,33 @@ moveJogador (Jogador (x,y)) j p =
         Move Direita -> if (x+1 >= length (snd (head p))) || (snd (p !! y) !! (x+1)) == Arvore then Jogador (x,y) else Jogador (x+1,y)
         Move Esquerda -> if (x-1 < 0) || (snd (p !! y) !! (x-1)) == Arvore then Jogador (x,y) else Jogador (x-1,y)
 
-animaObstaculos :: [(Terreno, [Obstaculo])] -> [(Terreno, [Obstaculo])]
+{- | A função __recursiva__ 'animaObstaculos' recebe uma lista de linhas do mapa, com ajuda da função auxiliar 'animaObstaculoAux' e através da velocidade v do terreno move todos os elementos da lista de obstáculos |v| unidades na direção da velocidade.
+
+=== Exemplo de utilização:
+
+>>>animaObstaculos [(Rio (-1), [Nenhum,Tronco,Tronco,Nenhum]),(Estrada 2, [Carro,Carro,Nenhum,Nenhum]),(Rio 1, [Tronco,Tronco,Nenhum,Nenhum])]
+[(Rio (-1),[Tronco,Tronco,Nenhum,Nenhum]),(Estrada 2,[Nenhum,Nenhum,Carro,Carro]),(Rio 1,[Nenhum,Tronco,Tronco,Nenhum])]
+-}
+animaObstaculos :: [(Terreno, [Obstaculo])] -- ^Linhas do mapa antes da animação.
+    -> [(Terreno, [Obstaculo])] -- ^Linhas do mapa depois do movimento dos obstáculos.
 animaObstaculos [] = []
 animaObstaculos (l:t) = 
     case fst l of
-        Relva -> l:t
+        Relva -> l:animaObstaculos t
         Rio v -> (Rio v, animaObstaculoAux v (snd l) 0):animaObstaculos t
         Estrada v -> (Estrada v, animaObstaculoAux v (snd l) 0):animaObstaculos t
 
-animaObstaculoAux :: Velocidade -> [Obstaculo] -> Int -> [Obstaculo]
+{- | A função auxiliar __recursiva__ 'animaObstaculoAux' recebe uma velocidade e uma lista de obstáculos, com esses valores "desvia" os elementos da lista pelo valor da velocidade.
+
+=== Exemplo de utilização:
+
+>>>animaObstaculoAux (-1) [Nenhum,Tronco,Tronco,Nenhum] 0
+[Tronco,Tronco,Nenhum,Nenhum]
+-}
+animaObstaculoAux :: Velocidade -- ^Velocidade do terreno a ser animado.
+    -> [Obstaculo] -- ^Lista de obstáculos a ser movimentados.
+    -> Int -- ^Contador. 
+    -> [Obstaculo] -- ^Obstáculos pós transformação no x.
 animaObstaculoAux v o n
     | v > 0 =
         if n < v then animaObstaculoAux v (last o:init o) (n+1) else o
